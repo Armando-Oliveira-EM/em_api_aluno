d079c097a781de7b0a4cd8cd0d4b7bb870f7908b
Armando-Oliveira-EM <armando.oliveira@escolarmanager.com.br>
2022-12-07 14:55:26 -0300

    first commit

---
+++ EM-Projeto/Controllers/AlunoController.cs
@@ -0,0 +1,98 @@
+﻿using EM_Projeto.Models;
+using EM_Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EM_Projeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AlunoController : Controller
+    {
+        public RepositorioAluno _repositorioAluno;
+
+        [HttpGet("/todos")]
+        public async Task<IEnumerable<Aluno>> GetAll()
+        {
+            _repositorioAluno = new RepositorioAluno();
+            return await _repositorioAluno.GetAll();
+        }
+
+        //[HttpGet("/verificacao")]
+        //public async Task<IEnumerable<Aluno>> Get(AlunoModel predicate)
+        //{
+        //    _repositorioAluno = new RepositorioAluno();
+        //    return await _repositorioAluno.Get();
+        //}
+
+        [HttpGet("/matricula")]
+        public async Task<Aluno> GetByMatricula(int matricula)
+        {
+            _repositorioAluno = new RepositorioAluno();
+            Aluno aluno = new()
+            {
+                Matricula = matricula
+            };
+
+            return await _repositorioAluno.GetByMatricula(aluno.Matricula);
+        }
+
+        [HttpGet("/partedonome")]
+        public async Task<IEnumerable<Aluno>> GetByContendoNoNome(string parteDoNome)
+        {
+            _repositorioAluno = new RepositorioAluno();
+            Aluno aluno = new()
+            {
+                Nome = parteDoNome
+            };
+            return await _repositorioAluno.GetByContendoNoNome(aluno.Nome);
+        }
+
+        [HttpPost("/adicionar")]
+        public async Task Add(AlunoModel alunoModel)
+        {
+            _repositorioAluno = new RepositorioAluno();
+            var aluno = new Aluno
+            {
+                Matricula = alunoModel.Matricula,
+                Nome = alunoModel.Nome,
+                CPF = alunoModel.CPF,
+                Nascimento = alunoModel.Nascimento,
+                Sexo = alunoModel.Sexo
+            };
+
+            await _repositorioAluno.Add(aluno);
+        }
+
+        [HttpPut("/atualizar")]
+        public async Task Update(AlunoModel alunoModel)
+        {
+            _repositorioAluno = new RepositorioAluno();
+            var aluno = new Aluno
+            {
+                Matricula = alunoModel.Matricula,
+                Nome = alunoModel.Nome,
+                CPF = alunoModel.CPF,
+                Nascimento = alunoModel.Nascimento,
+                Sexo = alunoModel.Sexo
+            };
+
+            await _repositorioAluno.Update(aluno);
+        }
+
+        [HttpDelete("/excluir")]
+        public async Task Remove(AlunoModel alunoModel)
+        {
+            _repositorioAluno = new RepositorioAluno();
+            var aluno = new Aluno
+            {
+                Matricula = alunoModel.Matricula
+            };
+
+            await _repositorioAluno.Remove(aluno);
+        }
+
+    }
+}
---
+++ EM-Projeto/Models/AlunoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EM_Projeto.Models
+{
+    public class AlunoModel
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+        public EnumeradorSexo Sexo { get; set; }
+    }
+}
